b0c0f81e8ec8c842685e1a7a9121c67828b14857
DrWizardDrunkenstein54 <48422595+DrWizardDrunkenstein54@users.noreply.github.com>
2022-02-09 19:24:20 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "../src/Components/Header";
+import Note from "../src/Components/Note";
+import Footer from "../src/Components/Footer";
+const React = require("react");
+
+function App() {
+    return(
+        <div>
+            <Header />
+            <Note />
+            <Footer />
+        </div>);
+}
+
+export default App;
